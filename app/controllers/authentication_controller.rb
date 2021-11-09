7b51c77c91dd984720144b19168a7b456138d44e
Hawleywoo <alhawley21@gmail.com>
2021-11-08 19:52:29 -0700

    token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,16 @@
 class AuthenticationController < ApplicationController
+
+    def login
+        @user = User.find_by(username: params[:username])
+
+        if !@user
+            render json: { message: "No user with that username"}, status: :unauthorized
+        else
+            if !@user.authenticate(params[:password])
+                render json: { error: 'Wrong password'}, status: :unauthorized
+            else
+                render json: { message: "logged in" }
+            end
+        end
+    end
 end
